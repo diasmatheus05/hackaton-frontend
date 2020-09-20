1532b1207287cb8fb6ffe86d015791784bce30d0
diasmatheus05 <diasmatheus05@gmail.com>
2020-09-20 18:20:50 -0300

    timer

--- src/pages/mainPage/components/studyBox/studyBox.js
+++ src/pages/mainPage/components/studyBox/studyBox.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import "./studyBox.css";
 
@@ -8,12 +8,33 @@ import pause_icon from '../../../../assets/pause.png';
 
 function StudyBox() {
     const [clicked, setClicked] = useState(false);
+    const [time, setTime] = useState(0);
+
+    // useEffect(() => {
+
+    //     setTime(time+1)
+    // }, [time])
+
+    // function timer() {
+    //     var aux = time + 1;
+
+    //     if (aux < 10) {
+    //         setTime("00:0" + aux + ":00");
+    //     } else if (aux > 60) {
+    //         aux = aux - 60;
+    //         setTime("01:" + aux + ":00");
+    //     } else {
+    //         setTime("00:" + aux + "00");
+    //     }
+        
+    //     setTimeout(timer(), 1000);
+    // }
 
     return (
         <div id="study-box" className="box">
             <div className="box-container">
                 {clicked 
-                    ? <p>00:40:07</p>
+                    ? <p>00:{time}:00 {setTimeout(()=> {setTime(time+1)}, 1000)}</p>
                     : <p>Iniciar <strong>estudo</strong></p>
                 }
                 <img src={clicked ? pause_icon : play_icon} alt="Play" onClick={() => setClicked(!clicked)} />
